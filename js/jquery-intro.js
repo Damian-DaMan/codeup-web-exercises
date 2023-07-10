8379bf9a34fbb1331ece8d3a9eae5e779e0d069b
Damian Solis <damianrsolis@gmail.com>
2023-07-10 09:03:34 -0500

    jQuery exercises

---
+++ js/jquery-intro.js
@@ -0,0 +1,58 @@
+"use strict";
+$(() => {
+
+
+// alert("the DOM is loaded");
+
+
+    // $("#id-container").css('background-color', 'red');
+    // document.querySelector("#id-container").style.backgroundColor = "blue";
+    //
+    //
+    // let titleElements = document.querySelectorAll('.title');
+    // titleElements.forEach(titleElements => {
+    //     titleElements.style.color = 'blue';
+    // });
+    //
+    //
+    // $('li').css('color', 'pink');
+    //
+    // $('.codeup').css()
+
+    // alert($('#id-container').html());
+    //
+    // const aboutList = $('#aList').html();
+    // alert(aboutList);
+
+
+    // $('.codeup').css('border', '1px dotted red');
+    //
+    // const codeUp = document.getElementsByClassName('codeup')
+
+    $('h1, p, li').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    // $('li').css('background-color', 'yellow');
+
+    // alert($('h1').html());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
