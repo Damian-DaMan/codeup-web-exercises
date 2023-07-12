41bd1760507c42aac41e063aa36a63dd422a6b89
Damian Solis <damianrsolis@gmail.com>
2023-07-12 09:44:41 -0500

    morning exercise along with tweaks from the walkthrough

--- js/jquery_faq_js.js
+++ js/jquery_faq_js.js
@@ -15,16 +15,62 @@ $(() => {
     //         $('#target-content').toggle();
     //     });
 
+// walk through using jQuery================
+    //add a button
+//     $('body').append(`
+//         <a href="#">Toggle Answers</a>
+//     `);
+//
+//
+//     $('a').on('click', () => {
+//         $('dd').toggleClass('invisible');
+//     })
+//
+//     //bonus
+//     $('dt').on('click, function() {
+//     $(this).toggleClass('highlighted')
+// });
+//
 
-    $(document).ready(function() {
-        $("#toggle-link").click(function(e) {
-            e.preventDefault();
-            // $("dd").slideToggle();
-            $("dd").toggleClass('invisible');
-            var linkText = $(this).text();
-            $(this).text(linkText === "Show Answers" ? "Hide Answers" : "Show Answers");
+
+    // JS VERSION OF DOING THE SAME THING
+    const togglevisibilityLink = document.createElement('a');
+    togglevisibilityLink.innerText = 'Toggle Answers';
+
+    const bodyElement = document.querySelector('body');
+    bodyElement.appendChild(togglevisibilityLink);
+
+    const ddElements = document.querySelectorAll('dd');
+    togglevisibilityLink.addEventListener('click', () => {
+        ddElements.forEach(ddElement) => {
+            ddElement.classList.toggle('invisible');
         });
     });
+
+
+
+
+    const dtElements = document.querySelectorAll('dt');
+    dtElements.forEach(dtElement) => {
+        dtElement.addEventListener('click', () => {
+            dtElement.classList.toggle('highlighted');
+        })
+    }
+
+
+
+
+
+//     MY VERSION OF DOING THIS
+    // $(document).ready(function() {
+    //     $("#toggle-link").click(function(e) {
+    //         e.preventDefault();
+    //         // $("dd").slideToggle();
+    //         $("dd").toggleClass('invisible');
+    //         var linkText = $(this).text();
+    //         $(this).text(linkText === "Show Answers" ? "Hide Answers" : "Show Answers");
+    //     });
+    // });
 
 
 
