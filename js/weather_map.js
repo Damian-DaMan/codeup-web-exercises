ee068204acf3dffa755736c4a1cdec99559110bb
Damian Solis <damianrsolis@gmail.com>
2023-07-17 15:39:27 -0500

    edited html, simplified .js

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,16 +3,33 @@ $(() => {
 
 
     // Global Variables
-
+    //     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     const map = initializeMap();
-    const marker = createMarker();
-    const popup = createPopup();
+    // const marker = createMarker();
+    // const popup = createPopup();
+    const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+    const SAN_ANTONIO_COORDS = [-98.4916, 29.4252];
+
+    const apiUrl = `${OPEN_WEATHER_URL}?lat=${SAN_ANTONIO_COORDS[1]}&lon=${SAN_ANTONIO_COORDS[0]}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+
+//  bread and butter to pull weather API data below
+    function getWeatherData() {
+        $.ajax({
+            url: apiUrl,
+            method: 'GET',
+        }).done((data) => {
+            renderWeatherCard(data);
+        })
+    }
+
+    function renderWeatherCard(data) {
+
+    }
 
 
 
 
-    // Functions
-
+    //     FUNCTIONS with MAPBOX
     function initializeMap() {
         mapboxgl.accessToken = MAPBOX_TOKEN;
 
@@ -26,24 +43,6 @@ $(() => {
         return new mapboxgl.Map(mapOptions);
     }
 
-    function createMarker() {
-        return new mapboxgl.Marker()
-            .setLngLat([-98.4916, 29.4260])
-            .addTo(map);
-    }
-
-    function createPopup() {
-        return new mapboxgl.Popup()
-            .setLngLat([-98.489615, 29.426827])
-            .setHTML(`
-                <div>
-                    <h1>Codeup</h1>
-                    <p>We can put anything we want</p>
-                </div>
-            `);
-    }
-
-
 
     // Events
 
@@ -52,44 +51,13 @@ $(() => {
 
 
 
+
+
+
     // Run at the start of page load
-    marker.setPopup(popup);
+    // marker.setPopup(popup);
     initializeMap();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    getWeatherData(SAN_ANTONIO_COORDS[1], SAN_ANTONIO_COORDS[0]);
 
 
 
