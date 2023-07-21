60d0da1d98670527e7847217a60a0a4ed044ede2
Damian Solis <damianrsolis@gmail.com>
2023-07-21 10:22:54 -0500

    went through walkthrough for map-filter-reduce

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -39,44 +39,67 @@
             yearsOfExperience: 9
         }
     ];
+
+
     // question 1 done ==================================================
+
+
     // question 2 done
-    const filteredUsers = users.filter(user => user.languages.length === 3);
-    console.log(filteredUsers);
+    const filteredUsers = users.filter(user => user.languages.length >= 3);
+    console.log('Users with the three languages => ', filteredUsers);
 
 
     // Question 3 done ==================================================
     const emailAddresses = users.map(user => user.email);
-    console.log(emailAddresses);
+    console.log('User emails => ', emailAddresses);
 
 
     // Question 4 done ==================================================
     // Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 
     // this will grab us the total years of experience from the users array
-    const totalYearsExperience = users.reduce((accumulator, user) => {
 
-        return accumulator + user.yearsOfExperience;
-    }, 0);
-    console.log(totalYearsExperience);
 
-    const averageYearsExperience = totalYearsExperience / users.length;
+    // const totalYearsExperience = users.reduce((accumulator, user) => {
+    //
+    //     return accumulator + user.yearsOfExperience;
+    // }, 0);
+    // console.log(totalYearsExperience);
+    //
+    // const averageYearsExperience = totalYearsExperience / users.length;
+    //
+    // console.log(averageYearsExperience);
 
-    console.log(averageYearsExperience);
+    // Walkthrough on #4
 
 
     // Question 5 =========================================================
-
+// focus on syntax first
     // console.log(totalYearsExperience);
-    const longestEmail = users.reduce((longest, user) => {
-        if (user.email.length > longest.length) {
-            return user.email;
-        } else {
-            return longest;
+    // const longestEmail = users.reduce((longest, user) => {
+    //     if (user.email.length > longest.length) {
+    //         return user.email;
+    //     } else {
+    //         return longest;
+    //     }
+    // }, '');
+    //
+    // console.log(longestEmail);
+
+
+    // walkthrough with Jason M.
+    const longest = users.reduce((accumulator, user, index) => {
+        const emailLength = user.email.length;
+        const accLength = accumulator.length;
+        if (emailLength > accLength) {
+            accumulator = user.email;
         }
+        return accumulator
     }, '');
 
-    console.log(longestEmail);
+    console.log('Longest email => ', longest);
+
+
 
 
     // Question 6 =========================================================
@@ -92,6 +115,21 @@
 
     console.log(`Your instructors are: ${namesString}`);
 
+    //Walkthrough.
+    // const usersString = users.reduce((accumulator, user, index) => {
+    //     if(index === (users.length - 1)) {
+    //         accumulator += `and ${user.name}.`
+    //     } else {
+    //         accumulator += `${user.name}, `
+    //     }
+    // }, 'Your instructors are: ');
+    //
+    // console.log(usersString);
+
+
+
+
+
     // Bonus =================================================================
 
     const languageList = users.reduce((accumulator, string, index) => {
@@ -100,7 +138,6 @@
 
         }
     })
-
 
 
 })();
